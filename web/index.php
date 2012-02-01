f0ba45927b782ee7cebf1d1fc3fc5b9ff6585aa1
Tomi Belan <tomi.belan@gmail.com>
2012-02-01 20:38:52 +0100

    kontrola require_https nezavisi od action.

--- web/index.php
+++ web/index.php
@@ -6,7 +6,7 @@ require 'config.php';
 
 
 function json_out($data) {
-  header('Content-Type: application/json');
+  header('Content-Type: application/json; charset=UTF-8');
   print json_encode($data);
 }
 function json_die($errtype) {
@@ -15,26 +15,25 @@ function json_die($errtype) {
 }
 
 
-$action = empty($_POST['action']) ? 'front' : $_POST['action'];
-
-
 if ($config->require_https) {
   if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') {
     // HTTPS je zapnute, ide sa dalej
     header('Strict-Transport-Security: max-age=500');
   }
-  else if ($action == 'front') {
-    // sme na frontpage, redirectneme z http na https
+  else if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    // mozme redirectnut z http na https
     header('Location: https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'], true, 301);
     exit();
   }
   else {
-    // sme na API callbacku, http je zakazane
-    json_die('https required');
+    // redirectom by vznikol GET, takze neredirectujeme, ale forbidneme
+    header('HTTP/1.1 403 Forbidden');
+    exit();
   }
 }
 
 
+$action = empty($_POST['action']) ? 'front' : $_POST['action'];
 $actions = array(
   'front' => 'src/front.php',
   'login' => 'src/login.php',
