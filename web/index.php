a24ebb131ff1d189fd914b860b73ff29fe0ef6e3
Tomi Belan <tomi.belan@gmail.com>
2012-03-26 14:42:49 +0200

    vyhodena vacsina kodu okolo https, o to nech sa stara webserver.

--- web/index.php
+++ web/index.php
@@ -15,21 +15,9 @@ function json_die($errtype) {
 }
 
 
-if ($config->require_https) {
-  if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') {
-    // HTTPS je zapnute, ide sa dalej
-    header('Strict-Transport-Security: max-age=500');
-  }
-  else if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    // mozme redirectnut z http na https
-    header('Location: https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'], true, 301);
-    exit();
-  }
-  else {
-    // redirectom by vznikol GET, takze neredirectujeme, ale forbidneme
-    header('HTTP/1.1 403 Forbidden');
-    exit();
-  }
+if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') {
+  // http://en.wikipedia.org/wiki/HTTP_Strict_Transport_Security
+  header('Strict-Transport-Security: max-age=500');
 }
 
 
