6c8c7717ffadf4bc24ad6befd1bee903e87c5b17
Martin Kralik <majak47@gmail.com>
2012-05-16 13:01:05 +0200

    Info o priebehu zmeneny na info o miestex.

---
+++ miesto.php
@@ -0,0 +1,18 @@
+<?php
+$page = 'miesto';
+require 'include/header.php';
+?>
+
+<h1>Miesto konania</h1>
+<p>
+Prijímačky budú prebiehať na fakulte matematiky, fyziky a informatiky Univerzity Komenského.<br/>
+Jej pozícia je spolu s dvomi najbližšími zastávkami mestskej hromadenej dopravy znáznornená na nasledujúcej mape.
+</p>
+<iframe width="640" height="480" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps/ms?msa=0&amp;msid=213107510519027086415.0004c0227e5ff63f4fbe0&amp;ie=UTF8&amp;t=h&amp;ll=48.151886,17.070737&amp;spn=0.013743,0.027466&amp;z=15&amp;output=embed"></iframe><br /><small>Pozrite si <a href="http://maps.google.com/maps/ms?msa=0&amp;msid=213107510519027086415.0004c0227e5ff63f4fbe0&amp;ie=UTF8&amp;t=h&amp;ll=48.151886,17.070737&amp;spn=0.013743,0.027466&amp;z=15&amp;source=embed" style="color:#0000FF;text-align:left">miesto konania prijímačok</a> na väčšej mape.</small>
+
+<p>
+Na vyhľadanie spojenia odporúčame použiť stránku <a href="http://imhd.zoznam.sk/ba/planovac-cesty-vyhladanie-spojenia.html" target="_blank">imhd.sk</a>.
+</p>
+<?php
+	require 'include/footer.php';
+?>
