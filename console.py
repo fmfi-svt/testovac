56775448a6e3af843238a03aa7afde3ad30904ed
Tomi Belan <tomi.belan@gmail.com>
2013-04-17 13:16:00 +0200

    console.py sa neda spustit pod Pythonom 3.
    casom by sice aj tak narazil na nejaky syntax error apod, ale medzitym by
    vyrobil zbytocne __pycache__ adresare s *.pyc subormi. ked vyhodi chybu
    hned v console.py, nestihne nic naimportovat.

--- console.py
+++ console.py
@@ -6,3 +6,6 @@ application = TestovacApp()
 if __name__ == '__main__':
     import sys
     sys.exit(application.run_command(sys.argv[1:]))
+
+# force a SyntaxError when using Python 3 by mistake
+if False: print "you must use Python 2 to run this"
