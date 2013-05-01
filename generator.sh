d4a43fbdd63192ea967ecc270494182f769a32ff
Richard Ostertag <ostertag@dcs.fmph.uniba.sk>
2013-05-01 07:43:48 +0200

    Ucesanie generator.sh z roku 2012.

--- generator.sh
+++ generator.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
-count=1416
-count=96
+stickers_needed=1400
+
+pages=$((stickers_needed/24))
+if [[ $((stickers_needed%24)) -gt 0 ]]
+	then
+	pages=$((pages+1))
+fi
+pages=$((pages+1))
+count=$((pages*24))
 
 rm -f tmp.txt
 > result.txt
 
 x=0
-while [ $x -lt $count ]
+while [[ $x -lt $count ]]
 do
 	dd if=/dev/urandom bs=8 count=200 2> /dev/null | od -tu8 -w8 | grep -P "^\d{7}\s+\d*[1-9]\d{13}$" | cut -c15-28 >> result.txt
 	sort result.txt | uniq > tmp.txt
 	mv tmp.txt result.txt
 	x=`cat result.txt | wc -l`
 done
+shuf result.txt > tmp.txt
+head -n$count tmp.txt > result.txt
 
-head -n$count result.txt > tmp.txt
-
-php appendVerhoeff.php < tmp.txt > result.txt
-rm tmp.txt
+php appendVerhoeff.php < result.txt > tmp.txt
+mv tmp.txt result.txt
 
 cat result.txt | php barQrCode3.php > stickers.tex
 
@@ -26,5 +33,9 @@ pdflatex --shell-escape stickers.tex
 rm stickers.aux
 rm stickers.log
 rm stickers-pics.pdf
+rm stickers.tex
 
-pdf2ps stickers.pdf
+pdftk stickers.pdf cat 2-$pages output stickers-cut.pdf
+mv stickers-cut.pdf stickers.pdf
+tail -n+25 result.txt > tmp.txt
+mv tmp.txt result.txt
