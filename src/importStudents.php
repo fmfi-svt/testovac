24cf2ca577c8ea91531102c7b38c3df6bd4a96d9
Jakub Marek <jakub.marek@gmail.com>
2013-06-07 17:00:35 +0200

    update import commandu

--- src/importStudents.php
+++ src/importStudents.php
@@ -1,26 +1,25 @@
 <?php
-
-require 'config.php';
-//Priezvisko;Meno;Datum nar.;III. Ročník;IV. Ročník;Forma
-//Abx;Marxx;4.10.19xx;1,81;2,15;denná
-//Adx;Martxx;2.4.19xx;2,55;1,3;denná
-//Adx;Filxx;26.9.19xx;2,18;2,18;denná
-
-$filename = "";
-
+ 
+require_once __DIR__. '/../config.php';
+//Priezvisko;Meno;Datum nar.;III. RoÄŤnĂ­k;IV. RoÄŤnĂ­k;Forma
+//Abx;Marxx;4.10.19xx;1,81;2,15;dennĂˇ
+//Adx;Martxx;2.4.19xx;2,55;1,3;dennĂˇ
+//Adx;Filxx;26.9.19xx;2,18;2,18;dennĂˇ
+$filename = $argv[1];
+ 
 $file = fopen($filename, "r");
 if ($file === false) {
     $output->writeln('<error>Failed to open file</error>');
     return;
 }
-
+ 
 $conn = connect_db();
 $line = fgets($file);
-
+ 
 try {
     while ($buffer = fgets($file)) {
         $split_line = preg_split("/[;]/", $buffer);
-
+ 
         $priezvisko = trim($split_line[0], "'\"");
         $meno = trim($split_line[1], "'\"");
         $datum = trim($split_line[2], "'\"");
@@ -28,26 +27,34 @@ try {
         $priemer2 = trim($split_line[4], "'\"");
         $split_line[5] = str_replace("\n", '', $split_line[5]);
         $forma = trim($split_line[5], "'\"");
-
-        $dateParts = preg_split("/[.]+/", $datum);
-        $datum = $dateParts[2] . '-' . $dateParts[1] . '-' . $dateParts[0];
+ 
+        if ($datum === '') {
+                $datum = '0000-00-00';
+        } else {
+                $dateParts = preg_split("/[.]+/", $datum);
+                $datum = $dateParts[2] . '-' . $dateParts[1] . '-' . $dateParts[0];
+        }
         $priemer1 = str_replace(',', '.', $priemer1);
         $priemer2 = str_replace(',', '.', $priemer2);
-        
-        $fields_values = array(
-            'meno' => $meno,
-            'priezvisko' => $priezvisko,
-            'datum_narodenia' => $datum,
-            'priemer1' => $priemer1,
-            'priemer2' => $priemer2,
-            'forma_studia' => $forma
-        );
-        $res = $conn->autoExecute('Students', $fields_values, DB_AUTOQUERY_INSERT);
+        if ($priemer1 == 0) {
+                $priemer1 = NULL;
+        }
+        if ($priemer2 == 0) {
+                $priemer2 = NULL;        
+        }
+        $stmt = $conn->prepare("insert into Students (meno,priezvisko,datum_narodenia,priemer1,priemer2,forma_studia) values (:meno,:priezvisko,:datum,:priemer1,:priemer2,:forma)");
+        $stmt->bindParam(':meno',$meno);
+        $stmt->bindParam(':priezvisko',$priezvisko);
+        $stmt->bindParam(':datum',$datum);
+        $stmt->bindParam(':priemer1',$priemer1);
+        $stmt->bindParam(':priemer2',$priemer2);
+        $stmt->bindParam(':forma',$forma);
+        $stmt->execute();
+       
     }
 } catch (Exception $e) {
     throw $e;
 }
-
-
+ 
 fclose($file);
 ?>
