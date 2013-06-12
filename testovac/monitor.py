55472a3e7bca079cb7e0b5b1598c154bd4d85f75
Richard Ostertag <ostertag@dcs.fmph.uniba.sk>
2013-06-12 12:29:18 +0200

    farby v monitore.

--- testovac/monitor.py
+++ testovac/monitor.py
@@ -3,6 +3,7 @@ import os
 import time
 from models import Users
 from .settings import exam
+from termcolor import colored
 
 
 columns = 2
@@ -27,23 +28,28 @@ def monitor(app):
     num = 0
     for row in results:
         their_time = now - row.begintime
+        delta_time = their_time
 
         if row.submitted:
-            subdesc = 'odovzdane  '
+            subdesc = colored('odovzdane  ', 'red', attrs=['bold'])
             num_submitted += 1
         elif their_time > exam.server_time_limit:
-            subdesc = 'expirovane '
+            subdesc = colored('expirovane ', 'red', attrs=['bold'])
             num_expired += 1
         else:
             subdesc = 'cas: %+03d:%02d' % (their_time // 60, their_time % 60)
 
         if row[1]:
             idledesc = '%+03d:%02d' % (row[3] // 60, row[3] % 60)
+            delta_time -= row[3]
         else:
-            idledesc = ' N/A '
+            idledesc = ' N/A  '
 
-        line = '{} {} odp: {:3d} (ev: {:3d} last: {})'.format(
-            row.pid, subdesc, row[2], row[1], idledesc)
+        line = '{} {} odp: {} (ev: {:3d} last: {})'.format(
+            row.pid, subdesc,
+            colored('%3d' % row[2], 'green', attrs=['bold']) if row[2] == 116 else '%3d' % row[2],
+            row[1],
+            colored(idledesc, 'yellow', attrs=['bold']) if delta_time > 10 * 60 else idledesc)
 
         num += 1
         if num % columns == 0:
@@ -55,8 +61,17 @@ def monitor(app):
     print ''
 
     print 'Celkovy pocet: %2d' % num
-    print ' - odovzdane : %2d' % num_submitted
-    print ' - expirovane: %2d' % num_expired
+
+    text = ' - odovzdane : %2d' % num_submitted
+    if num_submitted > 0:
+        text = colored(text, 'red', attrs=['bold'])
+    print text
+
+    text = ' - expirovane: %2d' % num_expired
+    if num_expired > 0:
+        text = colored(text, 'red', attrs=['bold'])
+    print text
+
     print ' - vyplna    : %2d' % (num - num_submitted - num_expired)
 
     db.close()
