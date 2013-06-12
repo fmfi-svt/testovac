2ece4b8657c39d49dc619ec2f8ba677ae1a2929e
Tomi Belan <tomi.belan@gmail.com>
2013-06-12 13:58:28 +0200

    utils/stat-display prerobeny na prikaz.

--- testovac/bucketstats.py
+++ testovac/bucketstats.py
@@ -1,9 +1,10 @@
 
 from fractions import Fraction
 from models import Questions, Users, Buckets, user_closed, get_results
+from subprocess import Popen, PIPE
 
 
-def bucketstats(app):
+def genstats(app):
     db = app.DbSession()
 
     pids = [user.pid for user in db.query(Users) if user_closed(user)]
@@ -18,17 +19,33 @@ def bucketstats(app):
         for (qid, qsubord), question_points in user_details.iteritems():
             sums[bucket_of[qid]] += question_points
 
-    print 'Bid,Size,Average reached,Maximum possible,Percent'
+    yield ('Bid', 'Size', 'Average reached', 'Maximum possible', 'Percent', 'Graph')
 
     for b in buckets:
         avg = float(sums[b.bid] / len(pids))
         max = b.size * b.points
-        print '%s,%s,%s,%s,%s' % (b.bid, b.size, avg, max, 100 * avg / max)
+        stars = '|' + ('*' * int(round(50 * avg / max))).ljust(50) + '|'
+        yield map(str, (b.bid, b.size, avg, max, 100 * avg / max, stars))
 
     db.close()
+
+
+def bucketstats(app):
+    for line in genstats(app):
+        print ','.join(line[:-1])
 bucketstats.help = '  $0 bucketstats > stats.csv'
+
+
+def bucketstars(app):
+    p = Popen(['column', '-s,', '-t'], stdin=PIPE)
+    for line in genstats(app):
+        p.stdin.write(','.join(line) + '\n')
+    p.stdin.close()
+    if p.wait() != 0: raise OSError("zly returncode")
+bucketstars.help = '  $0 bucketstars'
 
 
 commands = {
     'bucketstats': bucketstats,
+    'bucketstars': bucketstars,
 }
--- utils/hviezdy.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import sys
-
-print sys.stdin.readline()[:-1] + ',Graf'
-
-for line in sys.stdin:
-	fields = line.split(',')
-	num = int(round(float(fields[4]) / 100 * 50))
-	print (line[:-1] + ',|' + '*' * num + ' ' * (50-num) + '|') 
